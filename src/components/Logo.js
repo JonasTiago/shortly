9addeda02ab891691bcf17cb15b8f604ae7de256
Jonas Tiago <jonastiago95@gmail.com>
2022-12-22 18:27:19 -0300

    feat: logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import LOGOSVG from "./../assets/img/Vector.svg" 
+
+export default function Logo() {
+  return (
+    <LogoStyle>
+      <h1>Shortly</h1>
+      <img src={LOGOSVG} alt="fdsfsd" />
+    </LogoStyle>
+  );
+}
+
+const LogoStyle = styled.div`
+    margin:auto;
+    display:flex;
+    align-items:center;
+    h1{
+      font-size:4em;
+    }
+`
