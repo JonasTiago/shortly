7f6f4287d0ffbebaf1177bc7e46bdec8097465ff
Jonas Tiago <jonastiago95@gmail.com>
2022-12-22 18:29:20 -0300

    feat: signin page layout

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,27 @@
+import PageStyel from "../../assets/styles/page";
+import Form from "../../components/form";
+import Logo from "../../components/Logo";
+import NavBar from "../../components/Nav";
+
+export default function SignIn() {
+  return (
+    <>
+      <NavBar>
+        <a>Entrar</a>
+        <a>Cadastrar-se</a>
+      </NavBar>
+      <PageStyel>
+        <Logo />
+        <Form>
+          <label>
+            <input type="text" placeholder="E-mail" />
+          </label>
+          <label>
+            <input type="text" placeholder="Senha" />
+          </label>
+          <input type="submit" value="Entrar"/>
+        </Form>
+      </PageStyel>
+    </>
+  );
+}
