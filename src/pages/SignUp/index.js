d1f34890290d2229d87a041cae2f545a9e194b90
Jonas Tiago <jonastiago95@gmail.com>
2022-12-22 18:28:38 -0300

    feat: signup page layout

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,33 @@
+import PageStyel from "../../assets/styles/page";
+import Form from "../../components/form";
+import Logo from "../../components/Logo";
+import NavBar from "../../components/Nav";
+
+export default function SignUp() {
+  return (
+    <>
+      <NavBar>
+        <a>Entrar</a>
+        <a>Cadastrar-se</a>
+      </NavBar>
+      <PageStyel>
+        <Logo />
+        <Form>
+          <label>
+            <input type="text" placeholder="Nome" />
+          </label>
+          <label>
+            <input type="text" placeholder="E-mail" />
+          </label>
+          <label>
+            <input type="text" placeholder="Senha" />
+          </label>
+          <label>
+            <input type="text" placeholder="Confirmar senha" />
+          </label>
+          <input type="submit" value="Criar Conta"/>
+        </Form>
+      </PageStyel>
+    </>
+  );
+}
